e609ed6448bd2576f6aa206ce3a9d846cdbf94c0
Flavio Cattapan <flapanq@yahoo.com.br>
2015-06-23 22:26:59 -0300

    commit

---
+++ src/test/java/TesteJK.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TesteJK {
+
+	@Test
+	public void test() {
+		
+		Assert.assertTrue(true);
+	}
+
+}
